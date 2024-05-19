f5e5709ccc1148d0a0f22064fead21ea526d38a0
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-05-19 20:05:03 +0200

    fix config settings

--- config/database.js
+++ config/database.js
@@ -1,7 +1,29 @@
-const env = process.env.NODE_ENV || "development";
 const { Sequelize } = require("sequelize");
-const config = require("./config");
+require("dotenv").config();
 
-const sequelize = new Sequelize(config[env]);
+const sequelize = new Sequelize(
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  process.env.DB_NAME,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT,
+  }
+);
+
+async function initializeDatabase() {
+  try {
+    await sequelize.sync();
+    console.log("Database tables created!");
+
+    await sequelize.authenticate();
+    console.log("Successful connection");
+  } catch (error) {
+    console.error("Connection error", error);
+  }
+}
+
+initializeDatabase();
 
 module.exports = sequelize;
